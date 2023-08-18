fa2ea3764318ee5dd7400b9c6fee3f7deb514500
junior-brandao <juniorbrandaodev@gmail.com>
2023-08-18 15:41:33 -0300

    bCript password

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$FJ1cUO5wUvadD9tDcdU74Oczpc8Telgmik7zBxeMOeDuJwqd54BkC');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$FJ1cUO5wUvadD9tDcdU74Oczpc8Telgmik7zBxeMOeDuJwqd54BkC');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
